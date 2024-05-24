288fc5f6d074376aa4a0c8234a15cd442295e08e
Steven Enns <stevenaenns@gmail.com>
2024-05-24 13:37:13 -0700

    fix install file path

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 mkdir -p ~/bin
-python3 -m venv ~/countercat/venv
-source ~/countercat/venv/bin/activate
-cp src/countercat/*.py ~/bin/
+python3 -m venv ~/counter-cat/venv
+source ~/counter-cat/venv/bin/activate
+cp src/counter-cat/*.py ~/bin/
 chmod +x ~/bin/*.py
 pip3 install RPi.GPIO
 pip3 install discord.py
 # pip3 install PyBluez
-cp ~/countercat/etc/systemd/system/countercat.service /etc/systemd/system/countercat.service
+cp ~/counter-cat/etc/systemd/system/countercat.service /etc/systemd/system/countercat.service
