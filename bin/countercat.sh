65f082d51443ec361a66adb8c420f1e6cc5d1379
Steven Enns <stevenaenns@gmail.com>
2024-05-24 13:41:58 -0700

    fix countercat path

--- bin/countercat.sh
+++ bin/countercat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ~/countercat/venv/bin/activate
-python3 ~/countercat/src/countercat/discord-airhorn.py  --role honker --token OTQ2OTYyNTU4NjUxMzU5MzIy.GCCjQE.vnI6SM0nCndi9U6U405HxDF1U_fqy3At4dQuB4
+source ~/counter-cat/venv/bin/activate
+python3 ~/counter-cat/src/countercat/discord-airhorn.py  --role honker --token OTQ2OTYyNTU4NjUxMzU5MzIy.GCCjQE.vnI6SM0nCndi9U6U405HxDF1U_fqy3At4dQuB4
