c3d5672d8d0281087f4ef9e1950e35bbe564125f
Steven Enns <stevenaenns@gmail.com>
2024-06-04 19:34:56 +0100

    remove opencv dependency, accept image by http request body

--- src/gpt.py
+++ src/gpt.py
@@ -2,11 +2,15 @@ from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 import RPi.GPIO as GPIO
 import base64
-import cv2
+#import cv2
 import json
 import logging
 import requests
 import time
+
+pin = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
 
 # OpenAI API Key
 def payload(base64_image):
@@ -49,28 +53,17 @@ def encode_image(image_path):
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        cap = cv2.VideoCapture("rtsp://admin:Bsff7NUTWKUf@192.168.1.64:554/Streaming/Channels/101?transportmode=unicast&profile=Profile_1")
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-
-        ret, frame = cap.read()
-        if not cap.isOpened():
-            raise Exception("Error: Could not open RTSP stream")
-
-# Try to grab a frame
-        ret, frame = cap.read()
-        if not ret:
-            raise Exception("Error: Could not read frame from the stream: " + ret)
-
         image_path = f'./capture_{time.time()}.jpg'
-
-        cv2.imwrite(image_path, frame)
-
-# jrgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-# jimage_pil = Image.fromarray(image_rgb)
-# Path to your image
         logging.info(image_path)
-# Getting the base64 string
-        base64_image = encode_image(image_path)
+        content_len = int(self.headers.get('Content-Length'))
+        request_body = json.loads(self.rfile.read(content_len))
+
+        # logging.info(f'request_body: {request_body}')
+
+        base64_image = request_body['image']
+        # logging.info(f'base64_image: {base64_image}')
+        with open(image_path, 'wb') as imgf:
+            imgf.write(base64.b64decode(base64_image))
         api_key = "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"
         headers = {
           "Content-Type": "application/json",
@@ -84,14 +77,12 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         content = json.loads(response.json()['choices'][0]['message']['content'])
         if content['answer']:
             logging.info('honking the horn')
-            # requests.get("http://airhorn.my:8000/h1")
             GPIO.output(pin, 1)
-            time.sleep(secs)
+            time.sleep(0.1)
             GPIO.output(pin, 0)
             self.time_of_last_honk = time.time()
         else:
             logging.info('no cats on countertop')
-        cap.release()
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
