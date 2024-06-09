15e32a7215a3c3acf9d61b4a637b0bed17e9c868
Steven Enns <stevenaenns@gmail.com>
2024-06-09 15:37:51 +0100

    fix

--- bin/countercat.sh
+++ bin/countercat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ~/counter-cat/venv/bin/activate
-python3 ~/counter-cat/src/countercat/discord-airhorn.py  --role honker --token OTQ2OTYyNTU4NjUxMzU5MzIy.GCCjQE.vnI6SM0nCndi9U6U405HxDF1U_fqy3At4dQuB4
+# python3 ~/counter-cat/src/countercat/discord-airhorn.py  --role honker --token OTQ2OTYyNTU4NjUxMzU5MzIy.GCCjQE.vnI6SM0nCndi9U6U405HxDF1U_fqy3At4dQuB4
+python3 ~/counter-cat/src/gpt.py
