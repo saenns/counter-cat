c7638f5f7290643cce83e71e3d2c4bb67c68d082
Steven Enns <stevenaenns@gmail.com>
2024-06-09 15:37:51 +0100

    fix

--- bin/countercat.sh
+++ bin/countercat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ~/counter-cat/venv/bin/activate
-python3 ~/counter-cat/src/countercat/discord-airhorn.py  --role honker --token [REVOKED_DISCORD_BOT_TOKEN]
+# python3 ~/counter-cat/src/countercat/discord-airhorn.py  --role honker --token [REVOKED_DISCORD_BOT_TOKEN]
+python3 ~/counter-cat/src/gpt.py
